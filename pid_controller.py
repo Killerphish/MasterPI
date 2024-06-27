9163d2f7d89c07e5ebd330cc0633771b71d568fb
Killerphish <rlwinchester@gmail.com>
2024-06-27 10:26:18 -0400

    updates

---
+++ pid_controller.py
@@ -0,0 +1,16 @@
+class PIDController:
+    def __init__(self, kp, ki, kd, setpoint):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.setpoint = setpoint
+        self.previous_error = 0
+        self.integral = 0
+
+    def compute(self, current_value):
+        error = self.setpoint - current_value
+        self.integral += error
+        derivative = error - self.previous_error
+        output = self.kp * error + self.ki * self.integral + self.kd * derivative
+        self.previous_error = error
+        return output
