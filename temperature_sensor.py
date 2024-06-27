20ef384e51059ebeb2871b080cfbf2baee019b68
Killerphish <rlwinchester@gmail.com>
2024-06-27 10:51:33 -0400

    updates

--- temperature_sensor.py
+++ temperature_sensor.py
@@ -5,7 +5,7 @@ import adafruit_max31865
 class TemperatureSensor:
     def __init__(self, cs_pin):
         self.spi = board.SPI()
-        self.cs = digitalio.DigitalInOut(board.D18)
+        self.cs = digitalio.DigitalInOut(cs_pin)
         self.cs.direction = digitalio.Direction.OUTPUT
         self.sensor = adafruit_max31865.MAX31865(self.spi, self.cs, rtd_nominal=100, ref_resistor=430.0, wires=3)
     
