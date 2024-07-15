b62ed2cb6a3e5c0cf62c51bd47750eec7f53a46d
Killerphish <rlwinchester@gmail.com>
2024-07-15 16:24:09 -0400

    favicon fix

--- masterpi.py
+++ masterpi.py
@@ -247,6 +247,11 @@ def get_meater_status():
     }
     return jsonify(status)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 if __name__ == '__main__':
     try:
         app.run(host='0.0.0.0', port=5000)
