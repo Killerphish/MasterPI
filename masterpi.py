68105fa88f4b94d311e996e5139508c941048287
Killerphish <rlwinchester@gmail.com>
2024-07-18 15:47:57 -0400

    add render_template

--- masterpi.py
+++ masterpi.py
@@ -1,6 +1,6 @@
 import logging
 from logging.handlers import RotatingFileHandler
-from quart import Quart, jsonify, request
+from quart import Quart, jsonify, request, render_template  # Add render_template to the import
 from temperature_sensor import TemperatureSensor
 from pid_controller import PIDController
 from fan_control import FanController  # Import FanController class
