a74c269773c0324e439830ba5250bc40cda2de20
Killerphish <rlwinchester@gmail.com>
2024-08-20 12:37:47 -0400

    modal added to settings.html

--- static/js/settings.js
+++ static/js/settings.js
@@ -105,220 +105,10 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    const pidAutotuneButton = document.getElementById('pid_autotune');
-    if (pidAutotuneButton) {
-        pidAutotuneButton.addEventListener('click', function(event) {
-            event.preventDefault();
-            fetch('/pid_autotune', {
-                method: 'POST',
-                headers: {
-                    'X-CSRFToken': getCsrfToken()  // Include CSRF token
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    displayMessage('PID Autotune started! Waiting for completion...', 'success'); // Display success message
-                    checkAutotuneStatus();  // Call function to check autotune status
-                } else {
-                    displayMessage('Failed to start PID Autotune.', 'error'); // Display error message
-                }
-            })
-            .catch(error => {
-                console.error('Error starting PID Autotune:', error);
-                displayMessage('Error starting PID Autotune: ' + error.message, 'error'); // Display error message
-            });
-        });
-    }
-
-    function fetchSettings() {
-        fetch('/get_settings')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(settings => {
-                if (document.getElementById('device_name')) {
-                    document.getElementById('device_name').value = settings.device.name;  // Set default device name
-                }
-                if (document.getElementById('temp_unit')) {
-                    document.getElementById('temp_unit').value = settings.units.temperature || 'F'; // Default unit changed to Fahrenheit
-                }
-                updateSensorTemperatures(settings.sensors); // Update sensor temperatures
-            })
-            .catch(error => {
-                console.error('Error fetching settings:', error);
-            });
-    }
-
-    function fetchMeaterStatus() {
-        fetch('/get_meater_status')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                const meaterStatusElement = document.getElementById('meaterStatus');
-                if (meaterStatusElement) {
-                    if (data.connected) {
-                        meaterStatusElement.textContent = 'Meater is connected';
-                        meaterStatusElement.style.color = 'green';
-                    } else {
-                        meaterStatusElement.textContent = 'Meater is not connected';
-                        meaterStatusElement.style.color = 'red';
-                    }
-                } else {
-                    console.error('Element with id "meaterStatus" not found.');
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching Meater status:', error);
-                const meaterStatusElement = document.getElementById('meaterStatus');
-                if (meaterStatusElement) {
-                    meaterStatusElement.textContent = 'Error fetching Meater status';
-                    meaterStatusElement.style.color = 'red';
-                } else {
-                    console.error('Element with id "meaterStatus" not found.');
-                }
-            });
-    }
-
-    function updateFanStatus() {
-        fetch('/api/fan-status')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                const fanStatusElement = document.getElementById('fan-status');
-                if (fanStatusElement) {
-                    fanStatusElement.textContent = `Fan status: ${data.status}`;
-                } else {
-                    console.error('Element with ID "fan-status" not found.');
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching fan status:', error);
-                const fanStatusElement = document.getElementById('fan-status');
-                if (fanStatusElement) {
-                    fanStatusElement.textContent = 'Error fetching fan status';
-                } else {
-                    console.error('Element with ID "fan-status" not found.');
-                }
-            });
-    }
-
-    // Initialize Database Modal
-    const initDbButton = document.getElementById('initDbButton');
-    const initDbModal = document.getElementById('initDbModal');
-    const initDbStatus = document.getElementById('initDbStatus');
-
-    if (initDbButton) { // Check if initDbButton exists
-        initDbButton.addEventListener('click', function() {
-            initDbModal.style.display = 'block';
-            initDatabase();
-        });
-    }
-
-    window.addEventListener('click', function(event) {
-        if (event.target == initDbModal) {
-            initDbModal.style.display = 'none';
-        }
-    });
-
-    function initDatabase() {
-        fetch('/init_db', { method: 'POST' })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.status === 'success') {
-                    initDbStatus.textContent = 'Database initialized successfully.';
-                } else {
-                    initDbStatus.textContent = 'Error initializing database: ' + data.message;
-                }
-            })
-            .catch(error => {
-                initDbStatus.textContent = 'Error initializing database: ' + error.message;
-            });
-    }
-
-    // Ensure fetchStatus is called after the DOM is fully loaded
-    fetchMeaterStatus();
-    fetchSettings();
-    fetchStatus(); // Call the defined fetchStatus function
-    fetchMeaterTemperature(); // Added this line to call the new function
-    setInterval(fetchStatus, 3000); // Update status every 15 seconds
-
-    const saveIntegrationsButton = document.getElementById('saveIntegrations');
-    if (saveIntegrationsButton) {
-        saveIntegrationsButton.addEventListener('click', function(event) {
-            event.preventDefault();
-            saveIntegrationSettings();
-        });
-    }
-
-    function saveIntegrationSettings() {
-        const meaterEnabled = document.getElementById('meater_integration').checked;
-        const meaterUsername = document.getElementById('meater_email').value;
-        const meaterPassword = document.getElementById('meater_password').value;
-
-        const formData = new FormData();
-        formData.append('meater_enabled', meaterEnabled);
-        formData.append('meater_username', meaterUsername);
-        formData.append('meater_password', meaterPassword);
-
-        fetch('/save_integration_settings', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                // No need to display message here, as it will be handled server-side
-            } else {
-                console.error('Failed to save integration settings:', data.message);
-            }
-        })
-        .catch(error => {
-            console.error('Error saving integration settings:', error);
-        });
-    }
-
-    // Tab switching functionality
-    const tabLinks = document.querySelectorAll('.tab-link');
-    const tabContents = document.querySelectorAll('.tab-content');
-
-    tabLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const targetTab = this.getAttribute('data-tab');
-
-            // Remove active class from all tab contents
-            tabContents.forEach(content => {
-                content.classList.remove('active');
-            });
-
-            // Add active class to the target tab content
-            document.getElementById(targetTab).classList.add('active');
-        });
-    });
-
     const addSensorButton = document.getElementById('addSensor');
     const sensorList = document.getElementById('sensorList');
     const sensorTypeSelect = document.getElementById('sensor_type');
-    const editModal = document.getElementById('sensorEditModal');
     const removeModal = document.getElementById('sensorRemoveModal');
-    const editForm = document.getElementById('editSensorForm');
     const removeForm = document.getElementById('removeSensorForm');
 
     if (sensorTypeSelect) {
@@ -359,16 +149,17 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    editForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const index = this.elements.index.value;
-        const count = this.elements.count.value;
+    document.querySelectorAll('[id^="editSensorForm-"]').forEach(form => {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const index = this.elements.index.value;
 
-        fetch(`/edit_sensor/${index}`, {
-            method: 'POST',
-            body: new FormData(this)
-        }).then(() => {
-            window.location.reload();
+            fetch(`/edit_sensor/${index}`, {
+                method: 'POST',
+                body: new FormData(this)
+            }).then(() => {
+                window.location.reload();
+            });
         });
     });
 
