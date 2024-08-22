9a3729c3fb536c0debddf20613172721bad31a0b
Killerphish <rlwinchester@gmail.com>
2024-08-22 10:29:30 -0400

    update initialize_sensors

--- masterpi.py
+++ masterpi.py
@@ -105,7 +105,7 @@ def initialize_sensors(config):
                 spi = busio.SPI(clock=board.SCLK, MISO=board.MISO, MOSI=board.MOSI)
                 cs_pin = getattr(board, sensor_config['chip_select_pin'])
                 cs = digitalio.DigitalInOut(cs_pin)
-                sensor = MAX31856(spi, cs)
+                sensor = MAX31856(spi, cs, thermocouple_type=adafruit_max31856.ThermocoupleType.K)  # Ensure thermocouple type is set
                 sensor.temperature_unit = MAX31856.TEMPERATURE_CELSIUS  # Ensure the temperature unit is set correctly
                 sensors.append((sensor, temp_offset, enabled))
                 app.logger.info(f"Initialized {sensor_type} sensor on pin {sensor_config['chip_select_pin']}")
